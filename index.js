e7169fcc4b2cbc8f0a926149d17213c60a6b7f7c
shsobur <shsoburhossen951@gmail.com>
2024-05-30 21:16:02 +0600

    load data usering params for view details page

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
 // Mongodb Connection (-_-)
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4yea9q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,6 +34,13 @@ async function run() {
       const cursor = craftCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    })
+
+    app.get("/addcraft/:id", async(req, res) => {
+      const id = req.params.id;
+      const qurey = {_id: new ObjectId(id)}
+      const paint = await craftCollection.findOne(qurey);
+      res.send(paint);
     })
 
     app.post("/addcraft", async(req, res) => {
